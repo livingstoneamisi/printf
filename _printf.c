7c55fccdbce5271004b010cd1ea40625be447f71
livingstoneamisi <livingstoneamisi00@gmail.com>
2023-11-14 00:26:57 -0800

    task 1

--- _printf.c
+++ _printf.c
@@ -1,46 +1,62 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- * _printf - produces output according to a formet
- * @format: string
- *
- * Return: number of printer characters
+ * _printf - Printf function
  */
 int _printf(const char *format, ...)
 {
-	va_list printer;
-	int printno = 0, x = 0;
-	int (*f)(va_list);
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	if (format == NULL || format[0] == '\0')
-	{
+	if (format == NULL)
 		return (-1);
-	}
-	va_start(printer, format);
-	while (format[x] != '\0')
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[x] == '%')
+		if (format[i] != '%')
 		{
-			x++;
-			f = function_get(&format[x]);
-			if (f != NULL)
-			{
-				printno += f(printer);
-			}
-			else
-			{
-				write(1, "%", 1);
-				write(1, &format[x], 1);
-				printno += 2;
-			}
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
 		{
-			write(1, &format[x], 1);
-			printno++;
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
-		x++;
 	}
-	va_end(printer);
-	return (printno);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
