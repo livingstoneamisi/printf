daf207b98b2b4eaf68d9684f9b5c8ac55679a4a4
livingstoneamisi <livingstoneamisi00@gmail.com>
2023-11-13 22:50:33 -0800

    task 0

--- functions.c
+++ functions.c
@@ -80,46 +80,29 @@ int i_printer(va_list printer)
 }
 
 /**
- * print_binary - Prints an unsigned number
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Numbers of char printed.
+ * d_printer - prints an integer
+ * @printer: argument list
+ *
+ * Return: number of characters printed
  */
-int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int d_printer(va_list printer)
 {
-	unsigned int n, m, i, sum;
-	unsigned int a[32];
-	int count;
+	int a = va_arg(printer, int);
+	int b = count_digit(a);
+	int num = 0, tmp = a;
+	char c;
 
-	UNUSED(buffer);
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
-
-	n = va_arg(types, unsigned int);
-	m = 2147483648; /* (2 ^ 31) */
-	a[0] = n / m;
-	for (i = 1; i < 32; i++)
+	if (a < 0)
 	{
-		m /= 2;
-		a[i] = (n / m) % 2;
+		num += write_char('-');
+		tmp = -tmp;
 	}
-	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	while (b > 0)
 	{
-		sum += a[i];
-		if (sum || i == 31)
-		{
-			char z = '0' + a[i];
-
-			write(1, &z, 1);
-			count++;
-		}
+		c = (tmp / power(10, b - 1) + '0');
+		num += write_char(c);
+		tmp %= power(10, b - 1);
+		b--;
 	}
-	return (count);
-}
+	return (num);
+}	
